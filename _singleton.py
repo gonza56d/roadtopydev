da686258f1a2c7d1f5ee2fc7e95d69ccb11e67b2
Gonza Garcia <gonzalo.garcia01@globant.com>
2021-08-30 01:32:01 -0300

    singleton

--- _singleton.py
+++ _singleton.py
@@ -4,19 +4,14 @@ class Singleton:
 
     __shared_instance = None
 
-    @staticmethod
-    def get_instance():
-        if Singleton.__shared_instance is None:
-            Singleton()
-        return Singleton.__shared_instance
-
-    def __init__(self) -> None:
-        if Singleton.__shared_instance is not None:
-            raise Exception('This is a singleton class.')
-        Singleton.__shared_instance = self
+    def __new__(cls):
+        if cls.__shared_instance is not None:
+            return cls.__shared_instance
+        cls.__shared_instance = super().__new__(cls)
+        return cls.__shared_instance
 
 
-a = Singleton.get_instance()
-b = Singleton.get_instance()
-c = Singleton.get_instance()
-print(id(a), id(b), id(c))
+a = Singleton()
+b = Singleton()
+c = Singleton()
+print(a is b is c)
